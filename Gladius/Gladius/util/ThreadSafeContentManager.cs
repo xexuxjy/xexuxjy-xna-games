f6ebe56b7428390481874bd3e0e557818d0f01fe
markneale@hotmail.com <markneale@hotmail.com@d6d7f6a3-6031-1e73-d388-492d60fb64f1>
2013-08-19 21:36:00 +0000

    initial work on attack bar

--- Gladius/Gladius/util/ThreadSafeContentManager.cs
+++ Gladius/Gladius/util/ThreadSafeContentManager.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
-namespace IlluminatiEngine
+namespace Gladius.util
 {
     // based on ThreadSafeContentManager from http://konaju.com/?p=27
     public class ThreadSafeContentManager : ContentManager
     {
         static object loadLock = new object();
 
-        public ThreadSafeContentManager(IServiceProvider serviceProvider)
+        public ThreadSafeContentManager(Game game,IServiceProvider serviceProvider)
             : base(serviceProvider)
         {
+            m_game = game;
         }
 
-        public ThreadSafeContentManager(IServiceProvider serviceProvider, string rootDirectory)
+        public ThreadSafeContentManager(Game game, IServiceProvider serviceProvider, string rootDirectory)
             : base(serviceProvider, rootDirectory)
         {
+            m_game = game;
         }
 
         public override T Load<T>(string assetName)
@@ -28,5 +32,24 @@ namespace IlluminatiEngine
                 return base.Load<T>(assetName);
             }
         }
+
+        public  Texture2D GetColourTexture(Color color)
+        {
+            if (!m_colorMap.ContainsKey(color))
+            {
+                Texture2D newTexture = new Texture2D(m_game.GraphicsDevice, 1, 1);
+                Color[] colorData = new Color[1];
+                newTexture.GetData<Color>(colorData);
+                colorData[0] = new Color(color.ToVector3());
+                //colorData[0].A = 128;
+                newTexture.SetData(colorData);
+                m_colorMap[color] = newTexture;
+            }
+            return m_colorMap[color];
+        }
+
+        private Game m_game;
+        private Dictionary<Color, Texture2D> m_colorMap = new Dictionary<Color, Texture2D>();
+
     }
 }
