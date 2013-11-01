fc82045e16e42289171f71e2a8645001d8392b0b
xexuxjy@gmail.com <xexuxjy@gmail.com@d6d7f6a3-6031-1e73-d388-492d60fb64f1>
2013-11-01 11:48:29 +0000

    missing modeldata and content links

---
+++ trunk/Gladius/Gladius/renderer/ModelData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Gladius.renderer
+{
+    public class ModelData
+    {
+        public ModelData(Model model, Texture2D texture)
+        {
+            Model = model;
+            Texture = texture;
+            BoneTransforms = new Matrix[model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(BoneTransforms);
+        }
+
+
+        public Model Model;
+        public Texture2D Texture;
+        public Matrix[] BoneTransforms;
+    }
+}
