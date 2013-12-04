0d5c93c8dfba1343a0c901eb05b8b87c7969ce87
markneale@hotmail.com <markneale@hotmail.com@d6d7f6a3-6031-1e73-d388-492d60fb64f1>
2013-12-04 21:45:37 +0000

    placement screen

--- trunk/Gladius/Gladius/gamestatemanagement/screens/GladiatorPlacementScreen.cs
+++ trunk/Gladius/Gladius/gamestatemanagement/screens/GladiatorPlacementScreen.cs
@@ -6,11 +6,42 @@ using Gladius.gamestatemanagement.screenmanager;
 using Gladius.modes.shared;
 using Microsoft.Xna.Framework;
 using System.Diagnostics;
+using Microsoft.Xna.Framework.Graphics;
+using Gladius.renderer;
 
 namespace Gladius.gamestatemanagement.screens
 {
     public class GladiatorPlacementScreen : GameScreen
     {
+        public GladiatorPlacementScreen(List<CharacterData> chosenCharacters)
+        {
+        }
+
+        public override void LoadContent()
+        {
+            m_screenBackground = ContentManager.Load<Texture2D>("UI/backgrounds/PlainBackground");
+            m_smallFont = ContentManager.Load<SpriteFont>("UI/Fonts/DebugFont8");
+            m_headerFont = ContentManager.Load<SpriteFont>("UI/Fonts/UIFontLarge");
+        }
+
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            SpriteFont font = ScreenManager.Font;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Vector2 viewportSize = new Vector2(viewport.Width, viewport.Height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(m_screenBackground, viewport.Bounds, Color.White);
+
+            GraphicsHelper.DrawCenteredText(spriteBatch, m_headerFont, "Placement!", viewportSize / 2f, Color.White);
+
+            spriteBatch.End();
+
+        }
 
 
 
@@ -48,6 +79,9 @@ namespace Gladius.gamestatemanagement.screens
                 return m_characterMap;
             }
         }
+
+        private SpriteFont m_smallFont;
+        private SpriteFont m_headerFont;
 
         public Dictionary<Point,CharacterData> m_characterMap = new Dictionary<Point,CharacterData>();
         public List<CharacterData> m_characters = new List<CharacterData>();
