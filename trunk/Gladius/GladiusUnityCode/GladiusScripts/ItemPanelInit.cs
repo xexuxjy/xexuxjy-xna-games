4fcbc715264d9dc49eb2f51851b1cddcb8add4e2
xexuxjy@gmail.com <xexuxjy@gmail.com@d6d7f6a3-6031-1e73-d388-492d60fb64f1>
2014-06-27 13:52:26 +0000

    fixes with shop working

--- trunk/Gladius/GladiusUnityCode/GladiusScripts/ItemPanelInit.cs
+++ trunk/Gladius/GladiusUnityCode/GladiusScripts/ItemPanelInit.cs
@@ -8,29 +8,39 @@ namespace Gladius
     {
 
         public GameObject itemSlotPrefab;
+        dfControl lastControl;
+
+        dfRichTextLabel detailsPanelLabel;
+        dfControl imagePanel;
 
 
         // Use this for initialization
         void Start()
         {
+            GameObject panel = GameObject.Find("DetailsRTLabel");
+            detailsPanelLabel = panel.GetComponent<dfRichTextLabel>();
 
         }
 
         public void Populate()
         {
             List<string> shopItems = GladiusGlobals.CurrentShop.GetItemList();
-            dfScrollPanel scrollPanel = gameObject.GetComponent<dfScrollPanel>();
+            dfScrollPanel container = gameObject.GetComponent<dfScrollPanel>();
+            //dfControl container = gameObject.GetComponent<dfListbox>();
 
-            foreach (string itemName in shopItems)
+            if (container != null)
             {
-                GameObject shopItemGameObject = (GameObject)Instantiate(itemSlotPrefab);
-                shopItemGameObject.name = itemName;
-                ShopItemGUI shopItemGUI = shopItemGameObject.GetComponent<ShopItemGUI>();
-                shopItemGUI.ItemName = itemName;
+                foreach (string itemName in shopItems)
+                {
+                    GameObject shopItemGameObject = (GameObject)Instantiate(itemSlotPrefab);
+                    shopItemGameObject.name = itemName;
+                    ShopItemGUI shopItemGUI = shopItemGameObject.GetComponent<ShopItemGUI>();
+                    shopItemGUI.ItemName = itemName;
 
-                dfControl panel = shopItemGameObject.GetComponent<dfControl>();
-                scrollPanel.AddControl(panel);
-                shopItemGUI.Refresh();
+                    dfControl panel = shopItemGameObject.GetComponent<dfControl>();
+                    container.AddControl(panel);
+                    shopItemGUI.Refresh();
+                }
             }
 
         }
@@ -39,7 +49,42 @@ namespace Gladius
         // Update is called once per frame
         void Update()
         {
+            
+            dfControl controlUnderMouse = dfInputManager.ControlUnderMouse;
+            if (controlUnderMouse != null && controlUnderMouse != lastControl)
+            {
+                ShopItemGUI lastGuiItem = null;
 
+                GameObject go = controlUnderMouse.gameObject;
+                ShopItemGUI currentGuiItem = go.GetComponent<ShopItemGUI>();
+                
+                if (lastControl != null)
+                {
+                    lastGuiItem = lastControl.gameObject.GetComponent<ShopItemGUI>();
+                }
+
+                if (lastGuiItem != null)
+                {
+                    lastGuiItem.Selected = false;
+                }
+                if (currentGuiItem != null)
+                {
+                    currentGuiItem.Selected = true;
+                }
+
+                ItemSelectionChanged(lastGuiItem, currentGuiItem);
+
+                lastControl = controlUnderMouse;
+            }
         }
+
+        public void ItemSelectionChanged(ShopItemGUI previous, ShopItemGUI current)
+        {
+            if (previous != null && current != null)
+            {
+                detailsPanelLabel.Text = current.ItemName;
+            }
+        }
+
     }
 }
--- trunk/Gladius/GladiusUnityCode/GladiusScripts/LocalisationData.cs
+++ trunk/Gladius/GladiusUnityCode/GladiusScripts/LocalisationData.cs
@@ -11,12 +11,16 @@ namespace Gladius
 
         public void LoadExtractedData(String path)
         {
-            TextAsset textAsset = (TextAsset)Resources.Load("LocalisationData");
+            TextAsset textAsset = (TextAsset)Resources.Load("ExtractedData/LocalisationData");
             String data = textAsset.text;
             String[] lines = textAsset.text.Split(new String[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
             for (int counter = 0; counter < lines.Length; counter++)
             {
                 String line = lines[counter];
+                if (line.StartsWith("//") || line.StartsWith("#"))
+                {
+                    continue;
+                }
                 String[] lineTokens = line.Split('^');
                 this[int.Parse(lineTokens[0])] = lineTokens[3];
 
