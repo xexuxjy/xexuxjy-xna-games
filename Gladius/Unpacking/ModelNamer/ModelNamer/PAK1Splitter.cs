07c2b0d012436c23213182653552d5827e6442bf
xexuxjy@gmail.com <xexuxjy@gmail.com@d6d7f6a3-6031-1e73-d388-492d60fb64f1>
2014-10-21 16:49:51 +0000

    working version of decompressor for bec files

--- Gladius/Unpacking/ModelNamer/ModelNamer/PAK1Splitter.cs
+++ Gladius/Unpacking/ModelNamer/ModelNamer/PAK1Splitter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 namespace ModelNamer
 {
@@ -65,16 +67,69 @@ namespace ModelNamer
 
         static void Main(string[] args)
         {
-            String sourceDirectory = @"D:\gladius-extracted\ps2-decompressed\PAK1Animations";
-            String[] files = Directory.GetFiles(sourceDirectory, "*");
-            int counter = 0;
+            //String sourceDirectory = @"D:\gladius-extracted\ps2-decompressed\PAK1Animations";
+            //String[] files = Directory.GetFiles(sourceDirectory, "*");
+            //int counter = 0;
 
-            foreach (String file in files)
+            //foreach (String file in files)
+            //{
+            //    FileInfo sourceFile = new FileInfo(file);
+
+            //    new PAK1Splitter().Read(sourceFile, @"D:\gladius-extracted\ps2-decompressed\PAK1Animations-Unpacked");
+            //}
+
+            String inFile = @"C:\tmp\bec-extract\bec-extract\File6";
+            String outFile = inFile + "-out";
+            
+            byte[] largeArray = new byte[1024 * 1024];
+            using(BinaryReader binReader = new BinaryReader(new System.IO.FileStream(inFile, System.IO.FileMode.Open)))
             {
-                FileInfo sourceFile = new FileInfo(file);
+                byte[] sourceData = binReader.ReadBytes((int) binReader.BaseStream.Length);
+                MemoryStream ms = new MemoryStream(sourceData);
+                bool zlib = true;
+                Inflater inflater = new Inflater(!zlib);
+                InflaterInputStream inStream = new InflaterInputStream(ms, inflater);
+                //byte[] buf2 = new byte[largeArray.Length];
 
-                new PAK1Splitter().Read(sourceFile, @"D:\gladius-extracted\ps2-decompressed\PAK1Animations-Unpacked");
+                int currentIndex = 0;
+                int count = largeArray.Length;
+
+                try
+                {
+                    while (true)
+                    {
+                        int numRead = inStream.Read(largeArray, currentIndex, count);
+                        if (numRead <= 0)
+                        {
+                            break;
+                        }
+                        currentIndex += numRead;
+                        count -= numRead;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected exception - '{0}'", ex.Message);
+                    throw;
+                }
+
+
+
+                //List<byte> inputList = new List<byte>();
+                //inputList.AddRange(inputData);
+                //List<byte> outputList = ZlibDecoder.Inflate(inputList);
+                //byte[] outputData = outputList.ToArray();
+                using (System.IO.BinaryWriter outStream = new BinaryWriter(File.Open(outFile, FileMode.Create)))
+                {
+                    outStream.Write(largeArray,0,currentIndex);
+                }
+                new PAK1Splitter().Read(new FileInfo(outFile), @"C:\tmp\bec-extract\Unpacked");
             }
+
+
+            
+
+            
         }
     }
 
